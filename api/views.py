9b7a27b377c70c5e911d1b223eb99658e0184852
kotahorii <khorii.micron@gmail.com>
2021-10-26 01:40:21 +0900

    views.py created

--- api/views.py
+++ api/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import generics, viewsets
+from rest_framework import permissions
+from .serializers import TaskSerializer, PostSerializer, UserSerializer
+from rest_framework.permissions import AllowAny
+from .models import Task, Post
 
-# Create your views here.
+
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    permissions_classes = (AllowAny,)
+
+
+class PostListView(generics.ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permissions_classes = (AllowAny,)
+
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
