7f17bdb70a18aa351854c998e89513497175ea98
kotahorii <khorii.micron@gmail.com>
2021-10-26 01:36:04 +0900

    serializer created

--- api/models.py
+++ api/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Port(models.Model):
+class Post(models.Model):
     title = models.CharField(max_length=50)
     content = models.CharField(max_length=500)
     created_at = models.DateTimeField(auto_now_add=True)
